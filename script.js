d87438e75b49537426ed3c8be3259c2a6c6cca45
jamar gillard <jaydevelopment101@gmail.com>
2023-08-01 13:25:29 -0400

    console log bringing up search function, working good, goota make it real now. chagned to movies, api was easier. gotta figure out poster integration too

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
+// http://www.omdbapi.com/?apikey=442a3887&s=
+// 442a3887
+var movieListElem = document.querySelector('.post-list');
 
 
 
 
+//main(name)
+
+async function onSearchChange(event){
+    var search = document.getElementById('searchbox').value
+
+    const id = event.target.value;
+const movies = await fetch(`http://www.omdbapi.com/?apikey=442a3887&s=${search}`)
+const movieData = await movies.json();
+ console.log(movieData);
+//  movieListElem.innerHTML = movieData.map(post=> `
+//     <div class="post">
+//     <div class="post__title">
+//       ${post.title}
+//     </div>
+//     <p class="post__body">
+//       ${post.year}
+//     </p>
+//     <p class="post__body2">
+//     ${post.genre}
+//   </p>
+//     </div>
+//     `).join('');
+    }
 
 
 
 
+    // ??button 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//random
- 
-async function randomQuote(Event){
-    // const id = event.target.value;
-await fetch('https://animechan.xyz/api/random')
-    .then(response => response.json())
-    .then(quote => console.log(quote))
-}
