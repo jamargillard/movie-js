27a0bcfca04e43d25470967807c0fe9bc0e761fd
jamar gillard <jaydevelopment101@gmail.com>
2023-08-01 19:58:38 -0400

    did the js things, need to fix template literals and image, then finnish checklist

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // http://www.omdbapi.com/?apikey=442a3887&s=
 // 442a3887
-var movieListElem = document.querySelector('.post-list');
+var movieListElem = document.querySelector('#chibis');
 
 
 
 
-//main(name)
+//main 
 
 async function onSearchChange(event){
     var search = document.getElementById('searchbox').value
@@ -13,22 +13,125 @@ async function onSearchChange(event){
     const id = event.target.value;
 const movies = await fetch(`http://www.omdbapi.com/?apikey=442a3887&s=${search}`)
 const movieData = await movies.json();
- console.log(movieData);
-//  movieListElem.innerHTML = movieData.map(post=> `
-//     <div class="post">
-//     <div class="post__title">
-//       ${post.title}
-//     </div>
-//     <p class="post__body">
-//       ${post.year}
-//     </p>
-//     <p class="post__body2">
-//     ${post.genre}
-//   </p>
-//     </div>
-//     `).join('');
-    }
 
+var movieArray = [];
+for(var i in movieData)
+    movieArray.push([i, movieData [i]]);
+
+ console.log(movieData);
+
+// for(let i = 0; i >= 6; i++){
+ movieListElem.innerHTML = movieArray.map(movie=> `
+ 
+ <h1 class="movie__heading"> results for ${search}</h1>
+
+ <div class="chibi__container ">
+<div class="chibi__inner">
+
+
+<div class="goku">
+<img src="http://img.omdbapi.com/?apikey=442a3887&s=${search}" alt="" class="goku__pic">    <div class="post-list">
+        <div class="post">
+          <div class="post__title">
+             "${movie.title}"
+          </div>
+          <p class="post__body">
+             ${movie.year}
+          </p>
+          <p class="post__body2">
+            ${movie.genre}
+          </p>
+        </div>
+      </div>
+</div>
+
+
+<div class="naruto">
+<img src="http://img.omdbapi.com/?apikey=442a3887&s=${search}" alt="" class="naruto__pic">    <div class="post-list">
+        <div class="post">
+          <div class="post__title">
+                  "${movie.title}"
+          </div>
+          <p class="post__body">
+             ${movie.year}
+          </p>
+          <p class="post__body2">
+            ${movie.genre}
+          </p>
+        </div>
+      </div>
+</div>
+
+<div class="luffy">
+<img src="http://img.omdbapi.com/?apikey=442a3887&s=${search}" alt="" class="luffy__pic">    <div class="post-list">
+        <div class="post">
+          <div class="post__title">
+                 "${movie.title}"
+          </div>
+          <p class="post__body">
+             ${movie.year}
+          </p>
+          <p class="post__body2">
+            ${movie.genre}
+          </p>
+        </div>
+      </div>
+</div>
+
+<div class="goku">
+<img src="http://img.omdbapi.com/?apikey=442a3887&s=${search}" alt="" class="fast__pic">  <div class="post-list">
+        <div class="post">
+          <div class="post__title">
+                  "${movie.title}"
+          </div>
+          <p class="post__body">
+             ${movie.year}
+          </p>
+          <p class="post__body2">
+           ${movie.genre}
+          </p>
+        </div>
+      </div>
+</div>
+
+<div class="naruto">
+<img src="http://img.omdbapi.com/?apikey=442a3887&s=${search}" alt="" class="transformers__pic">  <div class="post-list">
+        <div class="post">
+          <div class="post__title">
+                  "${movie.title}"
+          </div>
+          <p class="post__body">
+             ${movie.year}
+          </p>
+          <p class="post__body2">
+            ${movie.genre}
+          </p>
+        </div>
+      </div>
+</div>
+
+<div class="luffy">
+<img src="http://img.omdbapi.com/?apikey=442a3887&s=${search}" alt="" class="gotg__pic">  <div class="post-list">
+        <div class="post">
+          <div class="post__title">
+                 "${movie.title}"
+          </div>
+          <p class="post__body">
+             ${movie.year}
+          </p>
+          <p class="post__body2">
+            ${movie.genre}
+          </p>
+        </div>
+      </div>
+</div>
+
+
+</div>
+</div>
+    `).join('');
+    }
+// }
 
 
 
