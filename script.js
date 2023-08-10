3690fcc66bacb3c3285010e5a806b62d2f2f61ff
jamar gillard <jaydevelopment101@gmail.com>
2023-08-10 17:52:37 -0400

    dumbing it down seemed to work, successfully finshed the project!(aside from corrections from FES), reworked project js, css, and html to make this new working model, css was actually the nightmare this time believe it or not.

--- script.js
+++ script.js
@@ -1,544 +1,83 @@
 
 
-// // 4th attempt, youtube video and stack overflow help
-
-// // variables
-const apikey = '442a3887&s';
-const apiurl = 'https://www.omdbapi.com/?apikey=';
 
 
-const urlWithKey = apiurl + apikey;
 
 
-let searchInput = document.getElementById('searchbox');
-let word = searchInput.value.trim();
-const searchBtn = document.getElementById('searchButton');
-// let savedSearchInput = '';
-const movieId = '';
 const loadingSpinEl = document.querySelector(' .loading')
-const searchTitleEl = document.querySelector(" #title__movie")
-const movieResultsEl = document.querySelector(" .movie__results")
 
+const moviesHTML = document.querySelector(".movie");
+let moviesList = [];
 
 
+document.addEventListener('keypress', function (e) {
+  if (e.keyCode === 13 || e.which === 13) {
+      e.preventDefault();
+      return false;
+  }
+  
+});
 
-// loading state after render movies func
-async function renderMovies(word){
-    setTimeout(() => {        
-        loadingSpinEl.classList += ' movies__loading';
-        }, 300)
 
+  document.querySelector('#movies').style.visibility = "hidden";
+  document.querySelector('.movie__container').style.visibility = "hidden";
 
 
 
-movieResultsEl.style.visibility='hidden';
-
-
-const movieTitles= await fetch (`https://www.omdbapi.com/?apikey=442a3887&s=${word}`);
-const movieTitlesData = await movieTitles.json();
-const movieTitlesArray = await movieTitlesData
-console.log(movieTitlesArray)
-
-
-setTimeout(() => {
-    movieResultsEl.style.visibility='hidden';
-// add {i} maybe
-
-
-movieResultsEl.innerHTML += movieHTML(movieTitlesArray)
-
-    // movieResultsEl.innerHTML = movieTitlesArray.map((movie) => movieHTML(movie)).join("");
-    // searchTitleEl.innerHTML = `Results for: ${word}` ;
-
-
-
-
-
-   
-
-
-
-    loadingSpinEl.classList.remove('movies__loading');
-
-
-}, 600);
-
-
-
-
-
-
-
-
-   
-
-
-    function onSearchChange(event){
-        word = event.target.value;
-        event.preventDefault();
-        renderMovies();
-    }
-   
-    function movieHTML(movie) {
-        return `<div class="movie">
-            <figure class="movie__cover--wrapper">
-                <img class="movie__cover" src="${movie.Poster}" alt="">
-            </figure>
-            <h2 class="movie__title">${movie.Title}</h2>
-            <p class="movie__year">${movie.Year}</p>
-        </div>
-        </div>`;
-        
-    }
-
-function titleHTML(word){
-  return  `Results for: ${word}`
-}
-//   search button (problem with undefined passing in renderMov dont worry for now)
-
-
-    const search = document.getElementById("searchBtn");
-    search.addEventListener("click", function () {
-    renderMovies(word);
-    });
-
-
-}
-
-
-
-
-    // hamburg
-    // function openMenu() {
-    //     document.body.classList += " menu--open"
-    // }
-   
-    // function closeMenu() {
-    //     document.body.classList.remove('menu--open')
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // const searchResults = dataFromFetch;
-
-
-
-
-
-
-// //       // // main things
-// // awaits the get movie function
-
-
-// document.addEventListener('DOMContentLoaded', async () => {
-//         const trendingMovieData = await Promise.all(trendingMovies.map(async movieIds => {
-//             const movieData = await getMovieId(movieId);
-//             return movieData;
-//         }));
-//         showMovieData(trendingMovieData);
-       
-//     })
-// // // function to get movie id , defined  with dataFromFetch
-
-
-// async function getMovieId(movieId){
-   
-//    const fetchResponse = await fetch(`${urlWithKey}&i=${movieId}&plot=short`);
-// const dataFromFetch = await fetchResponse.json();
-// return dataFromFetch;
-
-
-// }
-
-
-// // // search api for query, map over it (stack overflow encode), using data from fetch
-
-
-// //first part
-// const searchInputVal = searchInput.value.trim();
-
-
-// async function searchApiForInputVal(searchInputVal){
-//     const fetchResponse = await fetch(`${urlWithKey}&s=${encodeURIComponent(searchInputVal)}`)
-// const dataFromFetch = await fetchResponse.json();
-// console.log('API Response:', dataFromFetch);
-
-
-
-
-// // store results
-// const searchResults = dataFromFetch.Search || [];
-// console.log('Search Results:', searchResults);
-
-
-// const movieIds =  searchResults.map(movie => movie.imdbID);
-// console.log('Movie IDs:', movieIds);
-
-
-
-
-// const specificMovieData = await Promise.all(
-//     movieIds.map(async id => {
-//         const specificResponse = await fetch(`${urlWithKey}&i=${id}&plot=short`);
-//         const specificMovieData = await specificResponse.json();
-//         return specificMovieData;
-//     })
-// );
-
-
-// console.log('Specific Movie Data:', specificMovieData);
-// return specificMovieData.slice(0, 6);
-
-
-// }
-
-
-
-
-// function showMovieData(movieData){
-
-
-//     if (movieData.length === 0) {
-//         searchResults.innerHTML = 'No movies found.';
-//       } else {
-//         searchResults.innerHTML = `
-//         <h1 class="movie__heading"> results for ${search}</h1>
-       
-//         <div class="mov__container ">
-//          <div class="mov__inner">
-//         ${movieData
-//             .map(
-//         movie => `
-//          <div class="mov__pic--holder">
-//          <div class= 'mov__pic'>
-//          <img src="${movie.Poster}"  class="mov__pic" >
-//          </div>
-//          <div class="post-list">
-//                  <div class="post">
-//                    <div class="post__title">
-//                       "${movie.title}"
-//                    </div>
-//                    <p class="post__body">
-//                       ${movie.year}
-//                    </p>
-//                    <p class="post__body2">
-//                      ${movie.genre}
-//                    </p>
-//                  </div>
-//                </div>
-//          </div>
-       
-       
-//          <div class="mov__pic--holder">
-//          <div class= 'mov__pic2'>
-//          <img src="${movie.Poster}"  class="mov__pic">
-//          </div>
-//          <div class="post-list">
-//                  <div class="post">
-//                    <div class="post__title">
-//                       "${movie.title}"
-//                    </div>
-//                    <p class="post__body">
-//                       ${movie.year}
-//                    </p>
-//                    <p class="post__body2">
-//                      ${movie.genre}
-//                    </p>
-//                  </div>
-//                </div>
-//          </div>
-
-
-//                `
-//         )
-//              .join('')}
-//              </div>` ;
-
-
-//              const movieOptions = searchResults.querySelectorAll('.chibi__inner');
-//              movieOptions.forEach((option, index) => {
-//       option.addEventListener('click', () => {
-//         movieOption(movieData[index]);
-//       });
-//     });
-//   }
+async function mainRender(search) {
  
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-       
-// //        
-// //  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+
+  let movies = await fetch(
+    ` https://www.omdbapi.com/?i=tt3896198&apikey=442a3887&s=${search}`
+  );
+  let moviesData = await movies.json();
+  
+  moviesList = moviesData.Search;
+  moviesHTML.innerHTML = moviesData.Search.map((movie) =>
+    moviesShow(movie)
+  ).join("");
+}
+setTimeout(() => {        
+  loadingSpinEl.classList += ' movies__loading';
+  }, 300)
+
+  
+function searchInput(event) {
+ 
+  const search = event.target.value;
+
+  setTimeout(() => {        
+    loadingSpinEl.classList.remove('movies__loading');
+    document.querySelector('.movie__container').style.visibility = "visible";
+
+    }, 2000)
+    document.querySelector('#movies').style.visibility = "visible";
+
+  event.preventDefault();
+  mainRender(search);
+}
+
+function moviesShow(movie) {
+  return `
+<div class='movie__inner'>
+    <figure class="movie__cover--wrapper">
+      <img src="${movie.Poster}" alt="" class="movie__img"/>
+    </figure>
+    <div class="movie__description">
+    <p class="movie__title style="word-wrap: break-word; width: 100px">${movie.Title}</p>
+    <p class="movie__year">${movie.Year}</p>
+  </div>
+  </div>
+  `
+
+
+}
+mainRender();
+    
+
+  
+function showMovies() {
+  moviesHTML.style.display = "flex";
+}
 
